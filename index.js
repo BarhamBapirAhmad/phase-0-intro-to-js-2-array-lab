b109d3b91b5b295817b393692567387c0a9abd47
BarhamBapirAhmad <barhamabpirahmed@gmail.com>
2024-02-20 00:21:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+// Write your solution here
+var cats = [];
+cats.push('Milo', 'Otis', 'Garfield');
+
+function destructivelyAppendCat(name) {
+  cats.push('Ralph');
+  return cats;
+}
+function destructivelyPrependCat(name) {
+  cats.unshift('Bob');
+  return cats;
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+function appendCat(name) {
+  return [...cats, 'Broom'];
+}
+function prependCat(name) {
+  return ['Arnold', ...cats];
+}
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
